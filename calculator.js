3da4ebee81b3ff3d73e05dfd92286ff06437edc5
AdamG <adamglymph@gmail.com>
2022-01-05 20:21:42 +0900

    Added Clear and Backspace

--- calculator.js
+++ calculator.js
@@ -5,9 +5,13 @@ const equalsButton = document.querySelector('#equals');
 const clearButton = document.querySelector('#clear');
 const backButton = document.querySelector('#back');
 
-//string displayed to user
-let calculationString = " ";
-let currentOperand = null;
+
+let calculation = {
+    calculationString : "",
+    currentOperand : null,
+    operandIndex : 0,
+    canModify : true
+}
 
 operands.forEach(op => 
     op.addEventListener('click', 
@@ -18,33 +22,61 @@ numerals.forEach(num =>
     e => UpdateCalculationString(e.target.id)));
 
 equalsButton.addEventListener('click',OnEqualsPressed);
+clearButton.addEventListener('click', Clear);
+backButton.addEventListener('click', BackSpace);
 
 function UpdateDisplay(string){
     display.textContent = string;
 }
 
 function UpdateCalculationString(value){
-    calculationString += `${value}`;
-    UpdateDisplay(calculationString);
+    if(calculation.canModify)
+    {
+        calculation.calculationString += `${value}`;
+        UpdateDisplay(calculation.calculationString);
+    }     
 }
 
 function SetOperand(id){
-    if(currentOperand == null){
-        currentOperand = id;
+    if(calculation.currentOperand == null){
+        calculation.currentOperand = id;
+        calculation.operandIndex = calculation.calculationString.length;
         UpdateCalculationString(id);
     }
 }
 
 function OnEqualsPressed(){
-    if(currentOperand != null){
-        let opIndex = calculationString.indexOf(currentOperand);
-        let num1 = parseInt(calculationString.slice(0,opIndex));
-        let num2 = parseInt(calculationString.slice(opIndex+1));
+    if(calculation.currentOperand != null){
+        let num1 = parseInt(calculation.calculationString.slice(0,calculation.operandIndex));
+        let num2 = parseInt(calculation.calculationString.slice(calculation.operandIndex+1));
         if(isNaN(num1)) num1 = 0;
         if(isNaN(num2)) num2 = 0;
-        let answer = Operate(num1,num2,currentOperand);
-        UpdateDisplay(`${num1} ${currentOperand} ${num2} = ${answer}`);
+        let answer = Operate(num1,num2,calculation.currentOperand);
+        UpdateDisplay(`${num1} ${calculation.currentOperand} ${num2} = ${answer}`);
+        calculation.canModify = false;
     }
+}
+
+function Clear(){
+        calculation.calculationString = "";
+        resetOperand();
+        UpdateDisplay(calculation.calculationString);
+        calculation.canModify = true;
+}
+
+function BackSpace(){
+    if(!calculation.canModify)
+        return;
+    //check if removing the operand
+    if(calculation.calculationString.length-1 == calculation.operandIndex)
+        resetOperand();
+    calculation.calculationString = calculation.calculationString.slice(0,-1);
+    UpdateDisplay(calculation.calculationString);
+}
+
+function resetOperand(){
+    calculation.currentOperand = null;
+    calculation.operandIndex = 0;
 }
 
 function Operate (a,b,operand){
