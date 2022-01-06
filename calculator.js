2759cbfc9093df6ef66514a1cfbaaabbb3a4ee6a
AdamG <adamglymph@gmail.com>
2022-01-06 14:31:00 +0900

    Finsihed UI. Refactored JS to use an array.

--- calculator.js
+++ calculator.js
@@ -1,122 +1,127 @@
 const display = document.querySelector('.display');
-const numerals = document.querySelector('.numerals').childNodes;
-const operands = document.querySelector('.operands').childNodes;
+const numerals = document.querySelectorAll('.numerals');
+const operators = document.querySelectorAll('.operators');
 const equalsButton = document.querySelector('#equals');
 const clearButton = document.querySelector('#clear');
 const backButton = document.querySelector('#back');
+const negativeButton = document.querySelector('#negative');
+const decimalButton = document.querySelector('#decimal');
 
-
-let calculation = {
-    calculationString : "",
-    currentOperand : null,
-    operandIndex : 0,
-    canModify : true
-}
-
-operands.forEach(op => 
-    op.addEventListener('click', 
-    e => SetOperand(e.target.id)));
-
-numerals.forEach(num => 
-    num.addEventListener('click', 
-    e => UpdateCalculationString(e.target.id)));
-
+//connect buttons to events
+operators.forEach(op => op.addEventListener('click', e => SetOperator(e.target.id)));
+numerals.forEach(num => num.addEventListener('click', e => SetNumber(e.target.id)));
 equalsButton.addEventListener('click',OnEqualsPressed);
 clearButton.addEventListener('click', Clear);
 backButton.addEventListener('click', BackSpace);
+negativeButton.addEventListener('click',NegateNumber);
+decimalButton.addEventListener('click', SetDecimalPoint);
 
-function UpdateDisplay(string){
+//the calculation is held in an array. 
+//New digits are pushed into the current index as needed
+//num1, operation, num2
+let calculationArray = ["", "", ""];
+let currentCalculationIndex = 0;
+
+//show current calcution to display
+function UpdateDisplay(){
+    display.textContent = calculationArray.join("");
+}
+
+//send string to the display
+function SetDisplay(string){
     display.textContent = string;
 }
 
-function UpdateCalculationString(value){
-    if(calculation.canModify)
-    {
-        calculation.calculationString += `${value}`;
-        UpdateDisplay(calculation.calculationString);
-    }     
+//add digitto the calculation array
+function SetNumber(value){
+    calculationArray[currentCalculationIndex] += `${value}`;
+    UpdateDisplay();
 }
 
-function SetOperand(id){
-    if(calculation.currentOperand == null){
-        calculation.currentOperand = id;
-        calculation.operandIndex = calculation.calculationString.length;
-        UpdateCalculationString(id);
-    }
+//check for a decimal point before adding to the array
+function SetDecimalPoint(){
+    if(calculationArray[currentCalculationIndex].indexOf('.') ==-1)
+        SetNumber('.');
 }
 
+//toggle between positive and negative numbers
+function NegateNumber(){
+    let num = parseFloat(calculationArray[currentCalculationIndex]) 
+    num *= -1;
+    calculationArray[currentCalculationIndex] = `${num}`;
+    UpdateDisplay();
+}
+//set the operation for the calculation
+function SetOperator(id){
+    if(currentCalculationIndex == 0 || currentCalculationIndex == 1){
+        currentCalculationIndex = 2;
+        calculationArray[1] +=`${id}`;
+        UpdateDisplay();
+    } 
+}
+
+//when equals is pressed perform calculation,present result, and get ready for next calculation
 function OnEqualsPressed(){
-    if(calculation.currentOperand != null){
-        let num1 = parseInt(calculation.calculationString.slice(0,calculation.operandIndex));
-        let num2 = parseInt(calculation.calculationString.slice(calculation.operandIndex+1));
-        if(isNaN(num1)) num1 = 0;
-        if(isNaN(num2)) num2 = 0;
-        let answer = Operate(num1,num2,calculation.currentOperand);
-        UpdateDisplay(`${num1} ${calculation.currentOperand} ${num2} = ${answer}`);
-        calculation.canModify = false;
+    //check there is at least partial calculation
+    if(currentCalculationIndex == 2){
+        let num1 = parseFloat(calculationArray[0]);
+        let num2 = parseFloat(calculationArray[2]);
+        let answer = Operate(num1,num2,calculationArray[1]);
+      
+        calculationArray[0] = `${answer}`;
+        calculationArray[1] = "";
+        calculationArray[2] = "";
+        currentCalculationIndex = 0;
+        UpdateDisplay();
     }
 }
-
+//reset calculator
 function Clear(){
-        calculation.calculationString = "";
-        resetOperand();
-        UpdateDisplay(calculation.calculationString);
-        calculation.canModify = true;
+    calculationArray[0] = "";
+    calculationArray[1] = "";
+    calculationArray[2] = "";
+    currentCalculationIndex = 0;
+    SetDisplay("0");
 }
 
+//remove the current digit in the calculation
 function BackSpace(){
-    if(!calculation.canModify)
-        return;
-    //check if removing the operand
-    if(calculation.calculationString.length-1 == calculation.operandIndex)
-        resetOperand();
-    calculation.calculationString = calculation.calculationString.slice(0,-1);
-    UpdateDisplay(calculation.calculationString);
+    if(calculationArray[currentCalculationIndex].length > 0)
+    {
+        calculationArray[currentCalculationIndex] = calculationArray[currentCalculationIndex].slice(0,-1);
+    }
+    else if(currentCalculationIndex > 0)
+    {
+        currentCalculationIndex -= 1;
+        calculationArray[currentCalculationIndex] = calculationArray[currentCalculationIndex].slice(0,-1);
+    }
+    
+    UpdateDisplay();
 }
 
-function resetOperand(){
-    calculation.currentOperand = null;
-    calculation.operandIndex = 0;
+//round to 3 decimal places
+function round(num){
+    return Math.round(num*1000)/1000;
 }
 
-function Operate (a,b,operand){
-    switch (operand){
+//perform operation
+function Operate (a,b,operator){
+    if(isNaN(a)) a = 0;
+    if(isNaN(b)) b = 0;
+    switch (operator){
         case '+':
-            return Add(a,b);
+            return round(a+b);
             break;
         case '-':
-            return Subtract(a,b);
+            return round(a-b);
             break;
         case '*':
-            return Multiply(a,b);
+            return round(a*b);
             break;
         case '/':
-            return Divide(a,b);
+            return round(a/b);
             break;
         default:
             return "OOPS"
     }
 }
-//add
-function Add(a,b){
-    return a+b;
-}
-//subtract
-function Subtract(a,b){
-    return a-b;
-}
-//multiply
-function Multiply(a,b){
-    return a*b;
-}
-//divide
-function Divide(a,b){
-    return a/b;
-}
-
-
-
-
-
-
-//updateDisplay(operate(myOperation));
